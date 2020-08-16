b0336c7637680a828f4d8ef4ae5aa56bd21281e0
Taru-garg <tarugarg2@gmail.com>
2020-08-16 20:15:25 +0530

    T

--- server/new_policy.php
+++ server/new_policy.php
@@ -5,21 +5,21 @@
         header("Location:http://localhost/home");
         return;
     }
-    
+
     //Required Details
-    if(!isset($_POST['name']) || !isset($_POST['description']) 
+    if(!isset($_POST['name']) || !isset($_POST['description'])
     || !isset($_POST['maxsalary']) || !isset($_POST['locationavailable'])
     || !isset($_POST['link'])){
         $response["error"] = "Please fill in the details!";
         echo json_encode($response);
         return;
-    }  
+    }
 
-    //Sanitize string 
+    //Sanitize string
     $name = filter_var($_POST['name'],FILTER_SANITIZE_STRING);
     $description = filter_var($_POST['description'],FILTER_SANITIZE_STRING);
     $maxsalary = $_POST['maxsalary'];
-    $locationavailable = filter_var($_POST['locationavailable'],FILTER_SANITIZE_STRING);  
+    $locationavailable = filter_var($_POST['locationavailable'],FILTER_SANITIZE_STRING);
     $link = filter_var($_POST['link'],FILTER_SANITIZE_STRING);
 
     //Set Policy ID
@@ -31,13 +31,12 @@
     $insert = $mysqli->query("INSERT INTO policy(id,name,description,maxsalary,locationavailable) VALUES('$id','$name','$description',$maxsalary,'$locationavailable')");
     if($insert){
         $response["success"] = "Policy is added successfully!";
-        echo json_encode($response);
+        //echo json_encode($response);
+        header("location:admin.php");
         return;
-    }else{        
+    }else{
         $response["error"] = "We are having some trouble! Please try again later!";
         echo json_encode($response);
 
     }
-
-
 ?>
